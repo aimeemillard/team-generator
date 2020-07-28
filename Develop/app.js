f61790a832015e39edf53e05d5d6dc7757592f73
Aimee Millard <an.millard19@gmail.com>
2020-07-28 01:20:14 -0400

    update

--- Develop/app.js
+++ Develop/app.js
@@ -10,20 +10,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const allMembers = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
-var team = [
-    {
-        name:
-        role:
-        ID:
-        email:
-        officeNumber:
-      
-    }
-]
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -44,3 +34,152 @@ var team = [
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+function newMember() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "member",
+        message: "Which role has the new member?",
+        choices: ["Manager", "Engineer", "Intern", "I'm done adding members"],
+      },
+    ])
+    .then(info => {
+      if (info.member === "Manager") {
+        managerInfo();
+      } else if (info.member === "Engineer") {
+        engineerInfo();
+      } else if (info.member === "Intern") {
+        internInfo();
+      } else if (info.member === "I'm done adding members") {
+        generateHTML(outputPath, render(allMembers));
+      }
+    });
+}
+
+function managerInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter your name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter your id",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter your office number",
+      },
+    ])
+    .then(answers => {
+      let manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      allMembers.push(manager);
+      newMember();
+    });
+}
+
+function engineerInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter member's name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter member's ID number",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter member's email",
+      },
+      {
+        type: "input",
+        name: "user",
+        message: "Enter member's GitHub username",
+      },
+    ])
+    .then(answers => {
+      let engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.user
+      );
+      allMembers.push(engineer);
+      newMember();
+    });
+}
+
+function internInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter member's name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter member's ID number",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter member's email",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter member's school",
+      },
+    ])
+    .then(answers => {
+      let intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      allMembers.push(intern);
+      newMember();
+    });
+}
+
+function generateHTML(info, file) {
+  fs.writeFile(info, file, err => {
+    if (err) {
+      throw err;
+    }
+    console.log("Your team is complete!");
+  });
+}
+
+// function outputTeam() {
+//   if (!fs.existsSync(OUTPUT_DIR)) {
+//     fs.mkdirSync(OUTPUT_DIR);
+//   }
+//   fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+// }
+
+newMember();
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee.js");
+
+class Engineer extends Employee {
+  constructor(name, id, email, user) {
+    super(name, id, email);
+    this.user = user;
+  }
+
+  getGithub() {
+    return this.user;
+  }
+
+  getRole() {
+    return "Engineer";
+  }
+}
+
+module.exports = Engineer;
